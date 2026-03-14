33e3baab6efa128bae2d7faffadba42df6461e57
Vitaly Rubstein <vitaly@vitaly-mac.home>
2026-03-14 08:12:53 +0100

    Use separate favicons for dev and prod

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,17 @@ import "antd/dist/reset.css";
 import "./styles.css";
 import App from "./App";
 
+const faviconHref = import.meta.env.PROD ? "/favicon-prod.svg" : "/favicon-dev.svg";
+const faviconLink =
+  document.querySelector<HTMLLinkElement>("link[rel='icon']") ??
+  document.createElement("link");
+faviconLink.rel = "icon";
+faviconLink.type = "image/svg+xml";
+faviconLink.href = faviconHref;
+if (!faviconLink.parentElement) {
+  document.head.appendChild(faviconLink);
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
