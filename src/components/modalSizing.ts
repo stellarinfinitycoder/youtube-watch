146f03ddbe98a564ee3b0ad9653fca7ad8fdacf0
Vitaly Rubstein <vitaly@vitaly-mac.home>
2026-04-25 06:20:42 +0200

    Align summary page and modal widths

--- src/components/modalSizing.ts
+++ src/components/modalSizing.ts
@@ -2,4 +2,4 @@ export const RESPONSIVE_MEDIA_MODAL_WIDTH =
   "min(1680px, 92vw, max(0px, calc((100dvh - 300px) * 16 / 9)))";
 
 export const RESPONSIVE_SUMMARY_MODAL_WIDTH =
-  "min(1480px, 70vw)";
+  "min(1180px, 70vw)";
